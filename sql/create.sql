71fdde3fbff7aa94beeb3a7dbc58d63d7651382f
Dmitry <shiltsov@gmail.com>
2023-11-05 21:01:32 +0300

    final release

--- sql/create.sql
+++ sql/create.sql
@@ -1,16 +1,27 @@
 CREATE TABLE dogs
 (
-    pk          serial primary key,
+    pk          int NOT NULL,
     name        VARCHAR(64) not null,
     kind        VARCHAR(64) not null
 );
 
-INSERT INTO dogs(name, kind) 
+INSERT INTO dogs(pk, name, kind) 
 VALUES 
-    ('Bob','terrier'),
-    ('Marli', 'bulldog'),
-    ('Snoopy', 'dalmatian'),
-    ('Rex', 'dalmatian'),
-    ('Pongo', 'dalmatian'),
-    ('Tillman', 'bulldog'),
-    ('Uga', 'bulldog');
+    (0,'Bob','terrier'),
+    (1,'Marli', 'bulldog'),
+    (2,'Snoopy', 'dalmatian'),
+    (3,'Rex', 'dalmatian'),
+    (4,'Pongo', 'dalmatian'),
+    (5,'Tillman', 'bulldog'),
+    (6,'Uga', 'bulldog');
+
+CREATE TABLE timestamps
+(
+    id          int,
+    timestamp   int
+);
+
+INSERT INTO timestamps(id, timestamp)
+VALUES
+    (0, 12), (1,10)
+
